4b7007f760668454e9f1f81d3ec880afd18e1ad7
FOUILLET Guillaume - m089193 <guillaume.fouillet@soprasteria.com>
2019-05-23 18:26:51 +0200

    feat: Wrong get implementation
    with datarace

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"image"
+	"io"
 	"net/http"
 	"time"
 
@@ -14,6 +15,9 @@ type asciiRequest struct {
 	URL   string `json:"url"`
 	Width int    `json:"width"`
 }
+
+// Last image generated
+var lastGenerated string
 
 func downloadImage(url string) (*image.Image, error) {
 	response, err := http.Get(url)
@@ -29,8 +33,20 @@ func downloadImage(url string) (*image.Image, error) {
 
 	return &result, nil
 }
-
 func handleASCII(rw http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		handlePostASCII(rw, req)
+	case http.MethodGet:
+		handleGetASCII(rw, req)
+	}
+}
+func handleGetASCII(rw http.ResponseWriter, req *http.Request) {
+	// Naïve and wrong implementation
+	io.WriteString(rw, lastGenerated)
+}
+
+func handlePostASCII(rw http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
 	var data asciiRequest
 	err := decoder.Decode(&data)
@@ -38,7 +54,7 @@ func handleASCII(rw http.ResponseWriter, req *http.Request) {
 		http.Error(rw, "Wrong json request", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(rw, "Converting image from %q... \n", req.URL.Path, data.URL)
+	fmt.Fprintf(rw, "Converting image from %q... \n", data.URL)
 
 	start := time.Now()
 
@@ -53,14 +69,14 @@ func handleASCII(rw http.ResponseWriter, req *http.Request) {
 		http.Error(rw, fmt.Sprintf("Generating Ascii Art failed. %v", err), http.StatusInternalServerError)
 		return
 	}
-
-	fmt.Fprintf(rw, `
+	lastGenerated = fmt.Sprintf(`
 	Image from %q
 	Generated in %v
 
 	%s
 	`, data.URL, time.Since(start), string(result))
 
+	io.WriteString(rw, lastGenerated)
 }
 
 func main() {
