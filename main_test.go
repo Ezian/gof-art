b20d92e5b782fb7fee31a5c2d1afdea2f84e5601
FOUILLET Guillaume - m089193 <guillaume.fouillet@soprasteria.com>
2019-05-23 19:42:55 +0200

    feat: Test for datarace.

---
+++ main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+)
+
+const url = "http://localhost:9999/ascii"
+
+func postJSON() {
+
+	var jsonStr = []byte(`{"url":"https://2.bp.blogspot.com/-50t8QbXgxwI/WGgpaXNAYWI/AAAAAAAAEPE/SKJ-Bu12qpkrP7kklk1_QmWTehLoBRFcwCLcB/s1600/Gophers.jpg","width":80}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("[POST] error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("[POST] response Status:", resp.Status)
+}
+
+func getResult() {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("[GET] error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println("[GET] response Status:", resp.Status)
+}
+
+func runSeriesAsRoutine(f func(), count int, wait time.Duration, group *sync.WaitGroup) {
+	group.Add(1)
+	go func() {
+		defer group.Done()
+		for i := 0; i < 30; i++ {
+			f()
+			time.Sleep(wait)
+		}
+	}()
+}
+
+func TestDatarace(t *testing.T) {
+	// Run server
+	go main()
+
+	var group sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		runSeriesAsRoutine(postJSON, 10, 300*time.Millisecond, &group)
+	}
+	time.Sleep(5 * time.Second)
+
+	for i := 0; i < 5; i++ {
+		runSeriesAsRoutine(getResult, 15, 200*time.Millisecond, &group)
+	}
+
+	group.Wait()
+}
