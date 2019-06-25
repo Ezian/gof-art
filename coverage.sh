be303d1979f350f116e8844dfcb6c4f162c9fd9e
FOUILLET Guillaume - m089193 <guillaume.fouillet@soprasteria.com>
2019-06-25 03:02:29 +0200

    fix: Simplify Coverprofile in coverage test script

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-go test -coverprofile=cover.out -coverpkg=github.com/Ezian/gof-art/channel,github.com/Ezian/gof-art/mutex,github.com/Ezian/gof-art/naive,github.com/Ezian/gof-art/utils
+go test -coverprofile=cover.out -coverpkg=./...
 go tool cover -html=cover.out -o cover.html 
 browse cover.html
